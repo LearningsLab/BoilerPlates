576f6c75a29a2dea084cfaf3370f4a9cb368a37e
mkumar9009 <mkumar9009@gmail.com>
2021-10-08 19:50:27 +0530

    adding boiler plate of Splitwise

---
+++ Splitwise/client.java
@@ -0,0 +1,5 @@
+public class client {
+    public static void main() {
+        
+    }
+}
---
+++ Splitwise/models/Expense.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Expense {
+ 
+    
+    public static class Builder{
+
+        
+    }
+}
