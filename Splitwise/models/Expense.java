0b86f9ff0f2a910253d3649134e139189f0de18f
mkumar9009 <mkumar9009@gmail.com>
2021-10-09 21:56:40 +0530

    added model classes

--- Splitwise/models/Expense.java
+++ Splitwise/models/Expense.java
@@ -1,9 +1,55 @@
 package models;
+import java.util.Map;
+
+import Config.Constants;
 
 public class Expense {
  
-    
+    private Map <User,Float> contributors;
+    private Float amt;
+    private Map<User,Float> payees;
+    private Constants.SplitStrategy splitStrategy;
+    private Group grp;
+
+    public Float getExpenseAmt(){
+        return this.amt;
+    }
+    public Map<User,Float> getContributors(){
+        return this.contributors;
+    }
+    public Map<User,Float> getPayees(){
+        return this.payees;
+    }
+    public Constants.SplitStrategy getSplitStrategy(){
+        return this.splitStrategy;
+    }
+    public Group getExpenseGrp(){
+        return this.grp;
+    }
     public static class Builder{
+        private Expense expense;
+        public Builder(){
+            this.expense = new Expense();
+        }
+
+        public Builder setAmt(Float amt){
+            this.expense.amt=amt;
+            return this;
+        }
+
+        public Builder setContributors(Map<User,Float> contribs){
+            this.expense.contributors=contribs;
+            return this;
+        }
+
+        public Builder setpayees(Map<User,Float> borrowers){
+            this.expense.payees=borrowers;
+            return this;
+        }
+
+        public Expense buildExpense(){
+            return this.expense;
+        } 
 
         
     }
--- Splitwise/models/Group.java
+++ Splitwise/models/Group.java
@@ -1,5 +1,44 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Group {
+    private ArrayList<User> users;
+    private ArrayList<Expense> expenses;
+    private ArrayList<Txn> txns;
+    private Boolean isSetteled;
     
+    public Group (ArrayList<User> grpMembers){
+        this.users=grpMembers;
+    }
+
+    public ArrayList<Expense> getexpExpenses(){
+        return this.expenses;
+    }
+    public ArrayList<User> getGrpMembers(){
+        return this.users;
+    }
+    public ArrayList<Txn> getTxns(){
+        return this.txns;
+    }
+    public Boolean isSettled(){
+        return this.isSetteled;
+    }
+    public Expense addExpense(Expense expense){
+        this.expenses.add(expense);
+        return this.expenses.get(this.expenses.size()-1);
+    }
+    public ArrayList<User> adduser(User u){
+        this.users.add(u);
+        return this.users;
+    }
+    public Boolean settleup(){
+        //Code To be added here
+        
+        return this.isSetteled;
+    }
+
+
+
+
 }
